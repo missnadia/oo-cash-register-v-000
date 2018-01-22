20465f741480525457c3e7eca01cf41834e69435
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-22 22:48:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,9 @@ class CashRegister
 
   def add_item(title, price, quantity=1)
     self.total += price * quantity
-    quantity.times {items << title}
+    quantity.times do
+      items << title}
+    end
     self.last_transaction = price * quantity
   end
 
